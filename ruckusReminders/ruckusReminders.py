ef2437c3860a123b5ce0adbe2490b74fb4779f3e
dreamsofvinny <dreamsofvinny@gmail.com>
2021-06-17 14:35:48 -0500

    started event class

--- ruckusReminders/ruckusReminders.py
+++ ruckusReminders/ruckusReminders.py
@@ -1,3 +1,5 @@
+from datetime import date, time
+
 def convert(my_name):
     """
     Print a line about converting a notebook.
@@ -8,3 +10,38 @@ def convert(my_name):
     """
 
     print(f"I'll convert a notebook for you some day, {my_name}.")
+
+
+
+class Event:
+
+    def __init__(self, date_string, time_string, desc):
+        self.date = to_date(date_string)
+        self.time = to_time(time_string)
+        self.desc = desc
+
+
+    def to_date(date_string):
+        """
+        Converts month/day/year string into a date type
+        Args:
+            date_string (str): a string to convert
+        Returns:
+            the_date (date): the date for this event
+        """
+
+
+
+
+    def to_time(time_string):
+
+
+
+
+
+
+def main():
+
+
+if __name__ == '__init__':
+    main()
