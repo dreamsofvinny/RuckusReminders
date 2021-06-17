1a7189662634046d76be1c919d76453f63c4c888
dreamsofvinny <dreamsofvinny@gmail.com>
2021-06-17 14:49:26 -0500

    finished to_date method

--- ruckusReminders/ruckusReminders.py
+++ ruckusReminders/ruckusReminders.py
@@ -12,7 +12,6 @@ def convert(my_name):
     print(f"I'll convert a notebook for you some day, {my_name}.")
 
 
-
 class Event:
 
     def __init__(self, date_string, time_string, desc):
@@ -22,26 +21,23 @@ class Event:
 
 
     def to_date(date_string):
-        """
-        Converts month/day/year string into a date type
-        Args:
-            date_string (str): a string to convert
-        Returns:
-            the_date (date): the date for this event
-        """
-
-
-
+    """
+    Converts month/day/year string into a date type
+    Args:
+        date_string (str): a string to convert "MM/DD/YYYY"
+    Returns:
+        the_date (date): the date for this event
+    """
+    month, day, year = date_string.split("/")
+    the_date = date(int(year), int(month), int(day))
+    return the_date
 
     def to_time(time_string):
-
-
-
-
+        return 0
 
 
 def main():
+    to_date("05/29/1997")
 
-
-if __name__ == '__init__':
+if __name__ == '__main__':
     main()
